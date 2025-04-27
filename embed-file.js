4ebf6b166e46c2e4624db691f1702f79874786d5
Shiv Arora <shivsanarora22@gmail.com>
2025-04-27 22:25:02 +0530

    Changed file name

--- embed-file.js
+++ embed-file.js
@@ -1,5 +1,5 @@
 const { saveEmbedding } = require('./src/embed');
 
 (async () => {
-  await saveEmbedding('./data/Shiv_Arora_2121466_CSE_RESUME.pdf');  // Change to your file
+  await saveEmbedding('./data/your_file_name.pdf');  // Change to your file
 })();
